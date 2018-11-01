2b8b7581933447d5725deb963e43181e6254f5d2
Thompson Plyler <nealok.g@gmail.com>
2018-11-01 02:42:14 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  my_new_array = []
+end
+
+def array_with_two_elements
+  my_two_array = [1,2]
+end
+
+def first_element(element)
+
+element[0]
+
+end
+
+def third_element(element)
+  element[2]
+end
+
+def last_element(element)
+  element[-1]
+end
+
+def first_element_with_array_methods(element)
+  element.first
+end
+
+def last_element_with_array_methods(element)
+  element.pop
+end
+
+def length_of_array(element)
+  element.length
+end
